77fc49d8e6ced3135eaa965b9da6c1462b1e188b
DovydasKlisys232 <dovydasklisys@gmail.com>
2025-10-08 13:03:50 +0100

    update

--- src/Lab4/timer.c
+++ src/Lab4/timer.c
@@ -9,20 +9,50 @@
 //variables to store the time
 uint16_t count = 0;
 
-//internal interrupt
-//occurs when the timer capture occurs 
-ISR()
+ISR(INT0_vect)
 {
-
+	count++;
+	PORTB ^= (1 << 5);
 }
+
 
 int main(void)
 {
-	DDRB |= (1<<5);
+	usartInit();
+	usartSendString("Hello\n");
 	
+	DDRB |= (1<<5); //set led pin
+	DDRD &= ~(1<<2); //set pd2 as an input
+	PORTD |= (1 << 2); //enable internal pull up resistor on pd2
 
+	EICRA |= ((1 << ISC00) | (1 << ISC01)); //interrupt on rising edge
+    EIMSK |= (1 << INT0); //interrupt pin enable
+
+	//set the correct prescale (64)
+	TCCR1B |= ((1 << CS10) | (1 << CS11)); 
+	TCCR1B &= ~(1 << CS12);
+
+	sei();
+	char str[10];
+		
 	while(1)
 	{
-		
+
+		uint16_t oldCount;
+		uint16_t freq;
+	
+		if(TCNT1 == 15624) //if 1 second has passed
+		{
+			TCNT1 = 0;
+
+			freq = count - oldCount;
+
+			usartSendString("Freq: ");
+			sprintf(str, "%u\n", freq);
+			usartSendString(str);
+
+			oldCount = count;
+		}	
+
 	}
 }
