39456148ee490fd9816127b328afa6bae2065ff0
DovydasKlisys232 <dovydasklisys@gmail.com>
2025-10-15 12:52:30 +0100

    update

--- src/Lab4/timer.c
+++ src/Lab4/timer.c
@@ -1,38 +1,47 @@
+#define F_CPU 1000000UL
+
 #include <avr/io.h>
 #include<avr/interrupt.h>
 #include<stdio.h> //for sprintf
+#include <util/delay.h>
 #include "setup.h"
 #include "usart.h"
 
 //variables to store the time
 uint16_t count = 0;
 uint16_t freq = 0;
+uint8_t new_data_ready = 0;
 
 ISR(INT0_vect)
 {
 	count++;
 }
 
+ISR(TIMER1_COMPA_vect)
+{
+	freq = count;
+    count = 0;
+    new_data_ready = 1;
+}
+
 int main(void)
 {
-	usartInit();
+	
 	interrupt_init();
 	clock_init();
+	usartInit();
 	
 	sei();
-	char str[10];
+	char str[20];
 		
 	while(1)
 	{
-		if(TIFR1 & (1 << OCF1A)) //check if the timer interrupt occured
+		if(new_data_ready) //check if the timer interrupt occured
 		{
-			freq = count;
-			count = 0;
-			usartSendString("Freq: ");
-			sprintf(str, "%u\n", freq);
-			usartSendString(str);
-			TIFR1 = (1 << OCF1A ) ; // clear the CTC flag ( writing a logic one to the set flag clears it)
-		}	
-
+            sprintf(str, "Freq: %u\r\n", freq);
+            usartSendString(str);
+            new_data_ready = 0;
+		}
+		_delay_ms(250);
 	}
 }
