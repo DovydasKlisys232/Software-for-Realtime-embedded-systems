1b61e18dab4a5341f3a7ee012820d4c3755c8552
DovydasKlisys232 <dovydasklisys@gmail.com>
2025-11-10 17:34:30 +0000

    update

---
+++ Code/debug/src/blink.c
@@ -0,0 +1,16 @@
+#define F_CPU 1000000UL
+
+#include <avr/io.h>
+#include<avr/interrupt.h>
+#include<stdio.h> //for sprintf
+#include <util/delay.h>
+
+int main(void){
+	DDRB |= (1 << 2);
+
+	while(1)
+	{
+		PORTB ^= (1 << 2);
+	}
+	return 0;
+}
---
+++ Code/debug/src/hello.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int myGlobal = 5;
+
+int main()
+{
+	int myLocal = 0;
+	
+	printf("Hello world !\n");
+	
+	return 0;
+}
